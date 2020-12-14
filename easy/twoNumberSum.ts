6b9f29048d633c67913d58c465a53a92e40ebc7c
Erik De Luna <codingerik1.gmail.com>
2020-12-14 15:18:43 -0600

    adding twoSum algorithm

---
+++ easy/twoNumberSum.ts
@@ -0,0 +1,31 @@
+/**
+ * @function twoNumberSum 
+ * @param array - this input should be an array 
+ * @param targetSum - this input should be a number 
+ * 
+ * this function will return two numbers in the array that add up to the 
+ * target sum
+ * 
+ * if not * 
+ * 
+ * the the function will return an empty array
+ */
+
+
+export function twoNumberSum(array: number[], targetSum: number): number[] {
+    // Write your code here.
+    let table: { [key: number]: number } = {};
+    for (const num of array) {
+        const potentialNumber = targetSum - num;
+        if (potentialNumber in table) {
+            return [potentialNumber, num];
+        } else {
+            table[num] = num;
+        }
+    }
+    return []
+}
+
+
+// console.log(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10))
+
