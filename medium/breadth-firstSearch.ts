82ad330f7218101e4e62ec60e9dcb5e9c2f81619
Erik De Luna <codingerik1.gmail.com>
2020-12-22 03:28:56 -0600

    initial commit for breadthFirstSearch Algorithm

---
+++ medium/breadth-firstSearch.ts
@@ -0,0 +1,9 @@
+/**
+ * @Algorithm
+ * * BREADTH-FIRST-SEARCH *
+ * 
+ * @KEY - unlike the Depth-firstSearch
+ * the Breadth-First-Search cover the all 
+ * node in one level, before moving on to
+ * the next layer of depth
+ */
