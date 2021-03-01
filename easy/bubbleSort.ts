f9ddfae534d51c76803a3cdbc11a0c0224833943
Erik De Luna <codingerik1.gmail.com>
2021-03-01 15:47:57 -0600

    solving the bubble sort

--- easy/bubbleSort.ts
+++ easy/bubbleSort.ts
@@ -31,14 +31,10 @@ function bubbleSort(array: number[]) {
         }
         counter++;
     }
-
     // if swaps happen then we keep going 
-
     // if no swaps happen no need to iterate anymore and you can return
-
     return array;
 }
-
 
 function swap(i: number, j: number, array: number[]): void {
     const temp = array[j];
@@ -46,67 +42,39 @@ function swap(i: number, j: number, array: number[]): void {
     array[i] = temp;
 }
 
-
-
 let test: number[] = [8, 5, 2, 9, 5, 6, 3]; 
-
-console.log(bubbleSort(test)); 
-
-
-
-// function bubbleSort(arr: number[]): number[] {
-//     let isSorted = false; 
-//     let counter = 0; 
-//     while (!isSorted) {
-//         isSorted = true; 
-//         for (let i = 0; i < arr.length - 1 - counter ; i++) {
-
-//             if(arr[i] > arr[i + 1]){
-//                 swap(i, i + 1, arr)
-//                 isSorted = false
-//             }
-//         }
-//         counter++; 
-//     }
-//     return arr; 
-// }
-
-// // we made this swap function in order to organize the code a bit better
-// // this will help readability in the long run 
-// function swap(i: number, j: number, array: number[]): void {
-//     const temp = array[j];
-//     array[j] = array[i];
-//     array[i] = temp;
-// }
-
-
-// function bubbleSort(arr: number[]):number[] {
-//     let isSorted: boolean = false; 
-//     let counter: number = 0; 
-//     while(!isSorted){
-//         isSorted = true; 
-//         for (let i = 0; i < arr.length; i++) {
-
-//             if(arr[i] > arr[i + 1]){
-//                 swap(i, i + 1, arr); 
-//                 isSorted = false
-//             }
-//         }
-//         counter++; 
-//     }
-//     return arr; 
-// }
-
-// function swap(i: number, j: number, arr: number[]){
-//     // save the number in this index
-//     let number = arr[j]; 
-//     // assign the new number to the slot
-//     arr[j] = arr[i]; 
-//     // assign the saved number that WAS on the left to the right index slot
-//     arr[i] = number; 
-
-// }
-
-// let test: number[] = [8, 5, 2, 9, 5, 6, 3, 11]; 
-
 // console.log(bubbleSort(test)); 
+/**
+ * BubbleSort is one tool we can use when solving algorithms 
+ * we can use this tool to sort content
+ * O(n^2) quadratic => time complexity 
+ * the last number of the array of the first iteration always end up being in the correct place so 
+ * we start a counter so we don't have to look at it anymore 
+ * 
+ * Bubble Sort is a tool in our toolbox to use when solving algorightms 
+ */
+
+function bubbleSortTest(arr: number[]): number[]{
+    let wasSorted = false
+    let counter = 0
+    while(!wasSorted){
+        wasSorted = true; 
+        for (let i = 0; i < arr.length - 1 - counter; i++) {
+            if(arr[i] > arr[i + 1]){
+                swapTest(arr, i, i + 1);
+                wasSorted = false 
+            }   
+        }
+        // counter++; 
+    }
+    return arr; 
+}
+
+function swapTest(arr: number[], first: number, second: number):void{
+    let placeHolder = arr[second]
+    arr[second] = arr[first]; 
+    arr[first] = placeHolder; 
+    return 
+}
+
+console.log(bubbleSortTest(test)); 
